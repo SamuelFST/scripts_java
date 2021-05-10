9e7f38822d7be3b3603292630262163a8524fc76
Samuel Fernando <72414701+SamuelFST@users.noreply.github.com>
2021-05-10 20:26:15 -0300

    Add files via upload

---
+++ topicos_basicos/funcoes.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class funcoes {
+
+	public static void main(String[] args) {
+		Scanner read = new Scanner(System.in);
+		int num1, num2, num3;
+		
+		System.out.println("Digite 3 numeros: ");
+		num1 = read.nextInt();
+		num2 = read.nextInt();
+		num3 = read.nextInt();
+		int maior = comparacao(num1, num2, num3);
+		saida(maior);
+		read.close();
+	}
+	
+	public static int comparacao(int x, int y, int z) {
+		int vAux;
+		if(x > y && x > z) {
+			vAux = x;
+		}
+		else if(y > x && y >  z) {
+			vAux = y;
+		}
+		else {
+			vAux = z;
+		}
+		
+		return vAux;
+	}
+	
+	public static void saida (int maior) {
+		System.out.println("Maior: "+maior);
+	}
+
+}
