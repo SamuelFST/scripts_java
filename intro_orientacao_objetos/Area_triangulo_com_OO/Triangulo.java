e01d84933b6710566d6e00f7f0732ccaa0888863
Samuel Fernando <72414701+SamuelFST@users.noreply.github.com>
2021-05-13 21:11:46 -0300

    Update Triangulo.java

--- intro_orientacao_objetos/Area_triangulo_com_OO/Triangulo.java
+++ intro_orientacao_objetos/Area_triangulo_com_OO/Triangulo.java
@@ -2,4 +2,9 @@ package Area_triangulo_com_OO;
 
 public class Triangulo {
 	public double a, b, c;
+	
+	public double calculoArea() {
+		double p = (a + b + c)/2.0;
+		return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+	}
 }
