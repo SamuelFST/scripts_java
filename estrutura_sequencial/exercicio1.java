5b16eef2958ada215123372eb8b20d6d57b53a71
Samuel Fernando <72414701+SamuelFST@users.noreply.github.com>
2021-05-19 22:12:00 -0300

    Update exercicio1.java

--- estrutura_sequencial/exercicio1.java
+++ estrutura_sequencial/exercicio1.java
@@ -4,13 +4,13 @@ public class exercicio1 {
 	
 	public static void main(String[] args) {
 	Scanner read = new Scanner(System.in);
-	int a, b;
+	int num1, num2;
 	
 	System.out.print("Digite o primeiro numero inteiro: ");
-	a = read.nextInt();
+	num1 = read.nextInt();
 	System.out.print("Digite o segundo numero inteiro: ");
-	b = read.nextInt();
-	System.out.printf("SOMA = %d", (a+b));
+	num2 = read.nextInt();
+	System.out.printf("SOMA = %d", (num1+num2));
 	read.close();
 	}
 }
