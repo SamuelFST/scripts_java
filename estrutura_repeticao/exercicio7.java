4f36aae1d8f6688dfe6a5e4e555357cde7a70e75
Samuel Fernando <72414701+SamuelFST@users.noreply.github.com>
2021-05-23 22:37:32 -0300

    Update exercicio7.java

--- estrutura_repeticao/exercicio7.java
+++ estrutura_repeticao/exercicio7.java
@@ -6,13 +6,13 @@ public class exercicio7 {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner read = new Scanner(System.in);
-		int N;
+		int quantidadePares;
 		double valor1, valor2;
 		
 		System.out.print("Digite quantos pares para divisao deseja informar: ");
-		N = read.nextInt();
+		quantidadePares = read.nextInt();
 		
-		for(int i=0; i<N; i++) {
+		for(int i=0; i<quantidadePares; i++) {
 			System.out.printf("\nDigite os dois numeros do par %d: %n", i+1);
 			valor1 = read.nextDouble();
 			valor2 = read.nextDouble();
