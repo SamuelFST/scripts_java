626ac677912347ad63e2e3885e199c5fa9e0d943
Samuel Fernando <72414701+SamuelFST@users.noreply.github.com>
2021-07-27 20:49:11 -0300

    Update Main.java

--- continuacao_orientacao_objetos/contaBanco/Main.java
+++ continuacao_orientacao_objetos/contaBanco/Main.java
@@ -8,15 +8,43 @@ public class Main {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner read = new Scanner(System.in);
-		int numero;
-		String correntista;
-		double balanco;
+		ContaBancaria conta;
 		
-		System.out.println("Digite o numero da conta: ");
-		numero = read.nextInt();
+		System.out.print("Digite o numero da conta: ");
+		int numero = read.nextInt();
+		System.out.print("Digite o titular da conta: ");
 		read.nextLine();
-		System.out.println("Digite o numero do corretista: ");
-		correntista = read.nextLine();
+		String titular = read.nextLine();
+		
+		System.out.print("A conta tera um deposito inicial? (s/n)");
+		char opcao = read.next().charAt(0);
+		
+		if(opcao == 's') {
+			System.out.print("Digite o valor do deposito inicial: ");
+			double depositoInicial = read.nextDouble();
+			conta = new ContaBancaria(numero, titular, depositoInicial);
+		}
+		else {
+			conta = new ContaBancaria(numero, titular);
+		}
+		
+		System.out.println();
+		System.out.println("Dados da conta: ");
+		System.out.println(conta);
+		
+		System.out.println();
+		System.out.print("Digite um valor para deposito: ");
+		double valorDeposito = read.nextDouble();
+		conta.deposito(valorDeposito);
+		System.out.println("Dados atualizados da conta: ");
+		System.out.println(conta);
+		
+		System.out.println();
+		System.out.println("Digite um valor para saque: ");
+		double valorSaque = read.nextDouble();
+		conta.saque(valorSaque);
+		System.out.println("Dados atualizados da conta: ");
+		System.out.println(conta);
 		
 		read.close();
 	}
