15336220a378d03b870f5728737374490b539aae
Samuel Fernando <72414701+SamuelFST@users.noreply.github.com>
2021-08-02 21:23:26 -0300

    Update Funcionario.java

--- memoria_arrays_listas/listas/exercicio_lista/Funcionario.java
+++ memoria_arrays_listas/listas/exercicio_lista/Funcionario.java
@@ -12,7 +12,36 @@ public class Funcionario {
 	}
 	
 	public void aumentoSalarial(double porcentagem) {
-		
+		salario = salario + salario*porcentagem /100;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(Double salario) {
+		this.salario = salario;
+	}
+	
+	public String toString() {
+		return id +", " +nome +", " +String.format("%.2f", salario);
 	}
 	
 }
+
