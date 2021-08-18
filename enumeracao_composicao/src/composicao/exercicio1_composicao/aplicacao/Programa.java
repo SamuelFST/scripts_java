4cf1415997fb879162460a89da17cb95361a3987
Samuel Fernando <72414701+SamuelFST@users.noreply.github.com>
2021-08-18 18:36:34 -0300

    Update Programa.java

--- enumeracao_composicao/src/composicao/exercicio1_composicao/aplicacao/Programa.java
+++ enumeracao_composicao/src/composicao/exercicio1_composicao/aplicacao/Programa.java
@@ -1,19 +1,24 @@
 package composicao.exercicio1_composicao.aplicacao;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
+import composicao.exercicio1_composicao.entidades.ContratoHora;
 import composicao.exercicio1_composicao.entidades.Departamento;
 import composicao.exercicio1_composicao.entidades.Funcionario;
 import composicao.exercicio1_composicao.enums.NivelFuncionario;
 
 public class Programa {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
+		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
 		System.out.print("Digite o nome do departamento do funcionario: ");
 		String departamentoFuncionario = sc.nextLine();
@@ -30,15 +35,28 @@ public class Programa {
 		int quantidadeContratos = sc.nextInt();
 		
 		for(int i=0; i<quantidadeContratos; i++) {
-			System.out.printf("\nDigite os dados do contrato #%d", i+1);
+			System.out.printf("\nDigite os dados do contrato #%d\n", i+1);
 			System.out.print("Data (DD/MM/AAAA): ");
-			String data = sc.nextLine();
-			Calendar calendario = Calendar;
+			Date dataContrato = sdf.parse(sc.next());
 			System.out.print("Valor por hora: ");
-			
+			double valorHora = sc.nextDouble();
 			System.out.print("Duracao (horas): ");
+			int duracaoHoras = sc.nextInt();
+			
+			ContratoHora contrato = new ContratoHora(dataContrato, valorHora, duracaoHoras);
+			// Associando o contrato ao funcionario
+			funcionario.adicionarContrato(contrato);
 		}
 		
+		System.out.print("\nDigite o mes e ano para calcular o ganho (MM/YYYY):");
+		String mesAno = sc.next();
+		// Recortando a string informando o inicio e o fim da posição do recorte, e convertendo para int
+		int mes = Integer.parseInt(mesAno.substring(0, 2));
+		int ano = Integer.parseInt(mesAno.substring(3));
+		
+		System.out.println("Nome: "+funcionario.getNome());
+		System.out.println("Departamento: "+funcionario.getDepartamento().getNome());
+		System.out.printf("Renda de %s: %.2f", mesAno, funcionario.renda(ano, mes));
 		sc.close();
 	}
 
