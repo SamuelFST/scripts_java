5d6b0121f78db00406e10ff200ccee1f073f014e
Samuel Fernando <72414701+SamuelFST@users.noreply.github.com>
2021-08-31 17:51:54 -0300

    Update PedidoItem.java

--- enumeracao_composicao/src/exercicio_pedido_sumario/entidades/PedidoItem.java
+++ enumeracao_composicao/src/exercicio_pedido_sumario/entidades/PedidoItem.java
@@ -4,12 +4,15 @@ public class PedidoItem {
 	private Integer quantidade;
 	private Double preco;
 	
+	private Produto produto;
+	
 	public PedidoItem() {
 	}
 
-	public PedidoItem(Integer quantidade, Double preco) {
+	public PedidoItem(Integer quantidade, Double preco, Produto produto) {
 		this.quantidade = quantidade;
 		this.preco = preco;
+		this.produto = produto;
 	}
 
 	public Integer getQuantidade() {
@@ -28,13 +31,21 @@ public class PedidoItem {
 		this.preco = preco;
 	}
 
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+	
 	public Double subTotal() {
 		return quantidade * preco;
 	}
 
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
-		sb.append("NOME PRODUTO" +" , "+getPreco() +", "+getQuantidade() +", "+subTotal());
+		sb.append(produto.getNome() +", $"+getPreco() +", Quantidade: "+getQuantidade() +", Subtotal: "+subTotal());
 		return sb.toString();
 	}
 	
