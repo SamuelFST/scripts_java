7225e8a033aa7f8a857ee876fd24d8d7d5f86179
Samuel Fernando <72414701+SamuelFST@users.noreply.github.com>
2021-09-10 13:39:29 -0300

    Update Programa.java

--- heranca_polimorfismo/src/sobreposicao/Programa/Programa.java
+++ heranca_polimorfismo/src/sobreposicao/Programa/Programa.java
@@ -8,6 +8,10 @@ public class Programa {
 
 	public static void main(String[] args) {
 		
+		// Polimorfismo é algo que tem várias formas
+		// Abaixo todas as variáveis são do tipo Conta, mas são instânciadas como outro tipo
+		/* Isso faz com que mesmo elas sendo do mesmo tipo, a operação de saque tenha regras diferentes para cada uma
+		   já que o método de saque está sendo sobrescrito em cada uma das classes instânciadas */
 		Conta conta = new Conta(0001, "A", 1000.0);
 		Conta contaPoupanca = new ContaPoupanca(0002, "B", 1000.0, 0.05);
 		Conta contaEmpresarial = new ContaEmpresarial(0003, "C", 1000.0, 0.0);
